74162311c23866eb8930b499ef318e2849d4f19d
Bilha <32512727+bilha-analytics@users.noreply.github.com>
2019-11-08 12:03:13 +0300

    custom widgets at xml and code click img  button bg null

--- WidgetsApp/app/src/main/java/com/example/widgetsapp/DynamicUIActivity.kt
+++ WidgetsApp/app/src/main/java/com/example/widgetsapp/DynamicUIActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_dynamic_ui.*
 import kotlinx.android.synthetic.main.flag.view.*
 
@@ -17,6 +18,10 @@ class DynamicUIActivity : AppCompatActivity() {
             val v = layoutInflater.inflate(R.layout.flag, null) // set ViewGroup to null at init so tha edits can reflect
 //            v.findViewById<TextView>(R.id.txt_label).text = "I am # $i"
             v.txt_label.text = "I am # $i"
+            v.img_icon.setOnClickListener{
+                Log.d("Clicked", " item $i")
+                Toast.makeText(this, "Clicked item $i", Toast.LENGTH_SHORT).show()
+            }
             grid_dynamic.addView(v)
             Log.d("inflater", "Inflating flag # $i")
         }
