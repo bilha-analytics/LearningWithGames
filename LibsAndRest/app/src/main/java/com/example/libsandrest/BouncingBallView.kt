2e3aea50afdef3d6961b4cf22ab506a19bb50c23
Bilha <32512727+bilha-analytics@users.noreply.github.com>
2019-11-12 11:59:52 +0300

    basic animation concepts

--- LibsAndRest/app/src/main/java/com/example/libsandrest/BouncingBallView.kt
+++ LibsAndRest/app/src/main/java/com/example/libsandrest/BouncingBallView.kt
@@ -1,23 +1,28 @@
 package com.example.libsandrest
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RectF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import kotlin.random.Random
 
 class BouncingBallView(context: Context?, attrs: AttributeSet?) :
     View(context, attrs) {
 
     val painter = Paint()
+    var imgX = 0f
+    var imgY = 0f
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         if( canvas == null) return
         paintCircle(canvas)
+
+        // this is non-blocking on the UI
+        Thread(){
+            animateImg()
+        }.start()
     }
 
     fun paintCircle(canvas: Canvas){
@@ -27,6 +32,18 @@ class BouncingBallView(context: Context?, attrs: AttributeSet?) :
         painter.typeface = Typeface.create( Typeface.MONOSPACE, Typeface.BOLD_ITALIC)
         painter.textSize = 70f
         canvas.drawText("ABC", 5f, 105f, painter)
+
+        var img = BitmapFactory.decodeResource(resources, R.drawable.animals_game)
+        img = Bitmap.createScaledBitmap( img, 100, 100, false)
+        canvas.drawBitmap( img, imgX, imgY, null)
+    }
+
+    fun animateImg(){
+        imgX = Random.nextInt( 100 ).toFloat()
+        imgY = Random.nextInt( 100 ).toFloat()
+        // this is blocking
+        Thread.sleep( 2000 )
+        postInvalidate()
     }
 
 }
